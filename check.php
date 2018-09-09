da07325dcee3ae7d64ef5808436c4a77e7ef9967
RISHAB JAIN <36708743+rishabjn@users.noreply.github.com>
2018-09-09 17:35:41 +0100

    Add files via upload

---
+++ check.php
@@ -0,0 +1,52 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['username'])){
+	header('location:login.php');
+}
+
+$con = mysqli_connect('localhost', 'root');
+
+mysqli_select_db($con, 'quizdbase');
+
+if(isset($_POST['submit'])){
+
+	if(!empty($_POST['quizcheck'])){
+
+		$count = count($_POST['quizcheck']);
+		echo "Out of 5, you have selected ".$count." options";
+	$result=0;
+	$i=1;
+	$selected = $_POST['quizcheck'];
+	#print_r($selected);
+
+	$q = "select * from questions";
+	$query = mysqli_query($con,$q);
+
+
+	while ($rows = mysqli_fetch_array($query)){
+		print_r($rows['ans_id']);
+		 $checked = $rows['ans_id'] == $selected[$i];
+		 if($checked){
+		 	$result++;
+		 }
+		 $i++;
+	}
+
+	echo "<br> Your total score is " .$result;
+
+
+	}
+}
+
+	$name = $_SESSION['username'];
+	$finalresult = "insert into user(username,totalques,answerscorrect) values ('$name','5', '$result') ";
+	$queryresult = mysqli_query($con,$finalresult);
+
+
+
+?>
+
+
+
